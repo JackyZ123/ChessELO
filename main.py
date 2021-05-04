3e29dd6fd54ba4ffeef54e91bb851d4b68da60f8
Jacky Zhang <18504@burnside.school.nz>
2021-05-05 09:00:43 +1200

    Header

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("leaderboard.html", title="Home")
+    return render_template("leaderboard.html", title="Leaderboard")
 
 if __name__ == "__main__":
     app.run(debug=True)
