0b7c77d7708998f4e5ad6285065bbcec6c5868c8
Jacky Zhang <18504@burnside.school.nz>
2021-05-12 13:42:19 +1200

    side nav bar

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def leaderboard():
-    return render_template("leaderboard.html", title="Leaderboard")
+    return render_template("leaderboard.html", title="Leaderboard", group_name="my group")
 
 if __name__ == "__main__":
     app.run(debug=True)
