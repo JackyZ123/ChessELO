d08a5eff06bf07bc824f23be18fab9b6c7242181
Jacky Zhang <18504@burnside.school.nz>
2021-05-21 09:59:27 +1200

    inserted database

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ import sqlite3
 
 app = Flask(__name__)
 
+def select(txt):
+    con = sqlite3.connect("./static/RankSystem.db")
+    cursor = con.cursor()
+    cursor.execute(txt)
+    ret = cursor.fetchall()
+    con.close()
+    return ret
+
 @app.route('/')
 def leaderboard():
-    return render_template("leaderboard.html", title="Leaderboard", group_name="my group")
+    print(select())
+    return render_template("leaderboard.html", title="Leaderboard", group_name="my group", page=0)
 
 if __name__ == "__main__":
     app.run(debug=True)
